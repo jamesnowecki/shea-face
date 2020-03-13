d62b68e5db5dfb2aef93bcff6fa8dc16dbb40d1f
James Nowecki <James.Nowecki@Nology.local>
2020-03-13 10:10:25 +0000

    linked to github

---
+++ src/components/FaceCard/FaceCard.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import styles from "./FaceCard.module.scss";
+
+const FaceCard = () => {
+  return (
+    <>
+      <p>FaceCard works</p>
+    </>
+  );
+};
+
+export default FaceCard;
---
+++ src/components/FaceCard/FaceCard.test.js
@@ -0,0 +1,10 @@
+import React from "react";
+import FaceCard from "./FaceCard";
+
+describe("FaceCard tests", () => {
+  let component;
+
+  beforeEach(() => {
+    component =
+  })
+});
