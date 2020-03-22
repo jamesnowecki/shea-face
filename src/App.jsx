d2b636f5c82f770f165c584acacc775d435e7960
JAMES NOWECKI <JAMESNOWECKI@gmail.com>
2020-03-22 11:28:05 +0000

    api hooked in and mapping on buttonclick works

--- src/App.jsx
+++ src/App.jsx
@@ -2,19 +2,21 @@ import React from 'react';
 import logo from './logo.svg';
 import styles from './App.module.scss';
 import PortraitContainer from "./containers/PortraitContainer";
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 const App = () => {
 
   const [numberOfSheaFaces, updateFaceNum] = useState(0);
+  const [hallOfFaces, updateHallOfFaces] = useState([]);
+  useEffect(() => {getTheRandomNum()}, []);
+
 
   const getTheRandomNum = () => {
-  fetch('https://qrng.anu.edu.au/API/jsonI.php?length=10&type=uint8')
+  fetch('https://www.random.org/integers/?num=1&min=1&max=15&col=1&base=10&format=plain&rnd=new')
         .then(result => result.json())
         .then(result => {
-            let randomNum = result.data[0];
+            let randomNum = result;
             console.log(randomNum);
-            console.log(numberOfSheaFaces);
             updateFaceNum(randomNum);
             console.log(numberOfSheaFaces);
         })
@@ -23,29 +25,33 @@ const App = () => {
         });
       }
 
-      const defineSheasFace = (number) => {
-        getTheRandomNum();
-        printTheGallery(number)
+      const defineSheasFace = (num) => {
+        console.log(num)
+        printTheGallery(num)
       }
 
-  let sheaFaceArray = [];
-
-  const sheaFaceCard = <div><PortraitContainer /></div>;
 
   const printTheGallery = (numOfSheaFaces) => {
+
+    let sheaFaceArray = [];
+
+    const sheaFaceCard = <div><PortraitContainer /></div>;
+  
 
       for(let i = 0; i < numOfSheaFaces; i++){
         sheaFaceArray.push(sheaFaceCard);
       };
+
+      updateHallOfFaces(sheaFaceArray);
   };
 
   // printTheGallery(numberOfSheaFaces);
 
   return (
     <>
-    <button onClick={() => (defineSheasFace(numberOfSheaFaces))}>Print SheaFaces!</button>
+    <button onClick={() => (printTheGallery(numberOfSheaFaces))}>Print SheaFaces!</button>
     <div className={styles.app}>
-        {sheaFaceArray}
+        {hallOfFaces}
     </div>
     </>
   );
